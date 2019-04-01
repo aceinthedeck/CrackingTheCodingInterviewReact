f7a38e8c0353bc63c9e9cadacceb34672dc66b4e
Ace <ace@Admins-MacBook-Pro.local>
2019-04-01 16:05:48 +0100

    Updates

---
+++ precache-manifest.4a6e02c47ab7212919d05508d3f85f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecb3e6360d6d7b235792",
+    "url": "/CrackingTheCodingInterviewReact/static/js/runtime~main.acb9ae81.js"
+  },
+  {
+    "revision": "4a29fc0274febaff2e23",
+    "url": "/CrackingTheCodingInterviewReact/static/js/main.ce0e2b9c.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/CrackingTheCodingInterviewReact/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4a29fc0274febaff2e23",
+    "url": "/CrackingTheCodingInterviewReact/static/css/main.63a6d289.chunk.css"
+  },
+  {
+    "revision": "5426567aef07bdb772527e1e438b6204",
+    "url": "/CrackingTheCodingInterviewReact/index.html"
+  }
+];
